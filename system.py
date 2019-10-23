8145072e2a3d05eb9e2981f2abc51a083284f9ea
Andrea Denisse <crypto.andrea@protonmail.ch>
2019-10-23 12:08:28 +0100

    test1

--- system.py
+++ system.py
@@ -1,40 +1,11 @@
-from cmd import Cmd
 import os
 import string
+global args 
+args = ""
 
-class MyPrompt(Cmd):
-    prompt = 'pb> '
-    intro = "Greetings! Type ? to list commands"
-     
-    def do_exit(self, inp):
-        """exit the application. Shorthand: x q Ctrl-D."""
-        print("Exiting..")
-        return True
-             
-    def do_cd(self, inp):
-        """Change directory."""
-        #print("Changing directory... '{}'".format(inp))
-        os.chdir(inp)
-        
-    def do_pwd(self, inp):
-        """Get current directory!!"""
-        #print("Getting current directory..... '{}'".format(inp))
-        print(os.popen('pwd').read())
 
-    def help_help(self, inp):
-        """Get current directory!!"""
-        
-
-    def default(self, inp):
-        if inp == 'x' or inp == 'q':
-            return self.do_exit(inp)
-     
-        print("Unknown command: {}".format(inp.split(" ")[0]))
-     
-    do_EOF = do_exit
-     
 if __name__ == '__main__':
-    print("Reading profile...")
+    #print("Reading profile...")
     f = open('profile', "r")
     lines = f.readlines()
     if lines[0][:4] != "PATH":
@@ -46,12 +17,37 @@ if __name__ == '__main__':
     f.close()
     home = home.strip()
     pwd = os.popen('pwd').read()
-    print("Getting current directory...")
-    print(pwd)
-    print("Changing directory to HOME...")
+    #print("Getting current directory...")
+    #print(pwd)
+    #print("Changing directory to HOME...")
     os.chdir(home)
-    MyPrompt().cmdloop('Starting interpreter...')
     
+    def do_help():
+        for key, _ in list_commands.items():
+            print(key)
+
+    def do_ls():
+        os.system("ls")
+    
+    def do_cd():
+        global args
+        print("Changing directory... '{}'".format(args))
+        os.chdir(args[0])
+
+    list_commands = {"ls": do_ls, "cd": do_cd, "help": do_help, "?": do_help}
+    print("Greetings! Type ? or help to list commands")
+    while 1:
+        
+        inp = input("cmd> ")
+        
+        list_ = inp.split(" ")
+        command = list_[0]
+        args = list_[1:]
+        if command in list_commands:
+            list_commands[command]()
+        else:
+            print("unkonwn command..")
+            
     
     
  
